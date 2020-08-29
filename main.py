b4e533caa033a754b187b5239c7d79a6ef8c0e34
Eddieeeee <bridger.eddie@gmail.com>
2020-08-29 19:55:30 +0000

    Completed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import time
 import random
 Score = 0
-WordList1 = ["hot","summer","hard","dry","simple","light","weak","male","sad","win","small","ignore","buy","succeed","reject","prevent","exclude"]
-WordList2 = ["cold","winter","soft","wet","complex","darkness","strong","female","happy","lose","big","pay attention","sell", "fail","accept", "allow", "include"]
+WordList1 = ["hot","summer","hard","dry","simple","light","weak","male","sad","win","small","ignore","buy","succeed","reject","prevent","exclude","positive","thick","quiet","wrong","up"]
+WordList2 = ["cold","winter","soft","wet","complex","darkness","strong","female","happy","lose","big","pay attention","sell", "fail","accept", "allow", "include","negative","thin","loud","right","down"]
 q1 = []
 q2 = []
 for x in range(0,10):
@@ -14,6 +14,7 @@ for x in range(0,10):
     while equalCheck == True:
         number1 = random.randint(0,list1len-1)
         number2 = random.randint(0,list2len-1)
+        print(number1,number2)
         if number1 != number2:
             equalCheck = False
     q1.append(WordList1[number1])
@@ -27,10 +28,10 @@ for x in range(0,10):
         print("Correct, your score is now: \n",Score)
     else:
         print("Incorrect, the correct answer is: \n",q2[1])
-    del WordList1[number1]
-    del WordList1[number2]
-    del WordList2[number1]
-    del WordList2[number2]
+    WordList1.remove(q1[0])
+    WordList1.remove(q2[0])
+    WordList2.remove(q1[1])
+    WordList2.remove(q2[1])
 print ("Well done, your score is: \n",Score)
 
     
